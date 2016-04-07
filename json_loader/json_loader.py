4275ee7fbae5c6160ee80108117437c7b6e0f30f
Dongho Choi <dongho.j.choi@gmail.com>
2016-04-07 10:22:26 -0400

    Add reference

--- json_loader/json_loader.py
+++ json_loader/json_loader.py
@@ -5,10 +5,12 @@ Created on Thu Apr  7 09:43:04 2016
 @author: donghochoi
 """
 
+# API reference: http://pandas.pydata.org/pandas-docs/stable/generated/pandas.io.json.json_normalize.html
+# Online JSON Viewer: http://jsonviewer.stack.hu/
+
 import json
 from pandas.io.json import json_normalize
 
 
 json_parsed = json.load(open("working_set.json","r"))
-
 result = json_normalize(json_parsed, 'data')
